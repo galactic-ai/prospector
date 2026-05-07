c3ba7b6eee8f4cbebaf6a1484d779b4f1e5bba03
Nikhil Garuda <nikhilgaruda@live.com>
2026-05-07 14:53:40 -0500

    revert back fake fsps

--- prospect/sources/fake_fsps.py
+++ prospect/sources/fake_fsps.py
@@ -1,30 +1,35 @@
+import os
 import numpy as np
+from scipy.interpolate import interp1d
 
-__all__ = ["add_dust", "add_igm"]
-
-
-def add_dust(wave,specs,line_waves,lines,dust_type=0,dust_index=0.0,dust2=0.0,dust1_index=0.0,dust1=0.0,**kwargs):
-    """
-    wave: wavelength vector in Angstroms
-    specs: spectral flux density, in (young, old) pairs
-    line_waves: list of emission line wavelengths (Angstroms)
-    lines: emission line flux density, in (young, old) pairs
-    """
-
-    # Loop over the (young,old) pairs for both lines and continuum
-    for i, (spec, line) in enumerate(zip(specs,lines)):
-        if (i == 0): 
-            d1 = dust1
-        else:
-            d1 = 0.0
-
-        spec = attenuate(spec,wave,dust_type=dust_type,dust_index=dust_index,dust2=dust2,dust1=d1)
-        line = attenuate(line,line_waves,dust_type=dust_type,dust_index=dust_index,dust2=dust2,dust1=d1)
-
-    return specs, lines
+__all__ = ["add_dust", "add_igm", "agn_torus"]
 
 
-def attenuate(spec,lam,dust_type=0,dust_index=0.0,dust2=0.0,dust1_index=0.0,dust1=0.0):
+def add_dust(wave,specs,line_waves,lines,dust_type=0,dust_index=0.0,dust2=0.0,dust1_index=0.0,dust1=0.0,
+             frac_nodust=0,frac_obrun=0,
+             dust4_type=0,dust4_index=0.0,dust4=0.0,**kwargs):
+
+    d1_curve = attenuate(specs[0], wave, dust_type=dust_type, dust_index=dust_index, dust2=0, dust1_index=dust1_index, dust1=dust1, dust4=0.0)
+    cspi = specs[0]*d1_curve*(1-frac_obrun) + specs[0]*frac_obrun + specs[1]
+
+    diff_dust = attenuate(specs[1], wave, dust_type=dust_type, dust_index=dust_index, dust2=dust2, dust1=0.0,
+                          dust4_type=dust4_type, dust4_index=dust4_index, dust4=dust4)
+    specdust = (1-frac_nodust) * cspi*diff_dust + cspi*frac_nodust
+
+
+    # emission lines
+    d1_curve = attenuate(lines[0], line_waves, dust_type=dust_type, dust_index=dust_index, dust2=0, dust1_index=dust1_index, dust1=dust1, dust4=0.0)
+    ncspi = lines[0]*d1_curve*(1-frac_obrun) + lines[0]*frac_obrun + lines[1]
+
+    diff_dust = attenuate(lines[1], line_waves, dust_type=dust_type, dust_index=dust_index, dust2=dust2, dust1=0.0,
+                          dust4_type=dust4_type, dust4_index=dust4_index, dust4=dust4)
+    nebdust = ncspi*diff_dust*(1-frac_nodust) + ncspi*frac_nodust
+
+    return specdust, nebdust
+
+
+def attenuate(spec,lam,dust_type=0,dust_index=0.0,dust2=0.0,dust1_index=0.0,dust1=0.0,
+              dust4_type=0,dust4_index=0.0,dust4=0.0):
     """returns F(obs) / F(emitted) for a given attenuation curve + dust1 + dust2
     """
 
@@ -116,15 +121,23 @@ def attenuate(spec,lam,dust_type=0,dust_index=0.0,dust2=0.0,dust1_index=0.0,dust
         reddy = reddy/2.505
 
         attn_curve = dust2*reddy
-        
+
+    # -------------- AGN
+    ### power-law attenuation
+    attn_curve_4 = 0
+    if dust4_type != 0:
+        attn_curve_4 = (lam/lamv)**dust4_index * dust4
+
     dust1_ext = np.exp(-dust1*(lam/5500.)**dust1_index)
     dust2_ext = np.exp(-attn_curve)
+    dust4_ext = np.exp(-attn_curve_4)
 
-    ext_tot = dust2_ext*dust1_ext
-     
-    return ext_tot*spec
+    ext_tot = dust2_ext*dust1_ext *dust4_ext
 
-def add_igm(wave, spec, zred=None, igm_factor=None, add_igm_absorption=None):
+    return ext_tot
+
+
+def add_igm(wave, spec, zred=None, igm_factor=1.0, add_igm_absorption=None, **kwargs):
     """IGM absorption based on Madau+1995
     wave: rest-frame wavelength
     spec: spectral flux density
@@ -135,22 +148,22 @@ def add_igm(wave, spec, zred=None, igm_factor=None, add_igm_absorption=None):
     if add_igm_absorption == False:
         return spec
 
-    wave = np.asarray(wave) * (1+zred)
+    redshifted_wave = np.asarray(wave) * (1+zred)
 
     lylim = 911.75
     lyw = np.array([1215.67, 1025.72, 972.537, 949.743, 937.803, 930.748, 926.226, 923.150, 920.963, 919.352,918.129, 917.181, 916.429, 915.824, 915.329, 914.919, 914.576])
     lycoeff = np.array([0.0036,0.0017,0.0011846,0.0009410,0.0007960,0.0006967,0.0006236,0.0005665,0.0005200,0.0004817,0.0004487, 0.0004200,0.0003947,0.000372,0.000352,0.0003334,0.00031644])
     nly = len(lyw)
 
-    tau_line = np.zeros_like(wave)
+    tau_line = np.zeros_like(redshifted_wave)
     for i in range(nly):
         lmin = lyw[i]
         lmax = lyw[i]*(1.0+zred)
 
-        idx0 = np.where((wave>=lmin) & (wave<=lmax))[0]
-        tau_line[idx0] += lycoeff[i]*np.exp(3.46*np.log(wave[idx0]/lyw[i]))
+        idx0 = np.where((redshifted_wave>=lmin) & (redshifted_wave<=lmax))[0]
+        tau_line[idx0] += lycoeff[i]*np.exp(3.46*np.log(redshifted_wave[idx0]/lyw[i]))
 
-    xc = wave/lylim
+    xc = redshifted_wave/lylim
     xem = 1.0+zred
 
     idx = np.where(xc<1.0)[0]
@@ -167,4 +180,58 @@ def add_igm(wave, spec, zred=None, igm_factor=None, add_igm_absorption=None):
 
     # attenuate the input spectrum by the IGM
     # include a fudge factor to dial up/down the strength
-    return spec*np.exp(-tau*factor)
+    res = spec*np.exp(-tau*igm_factor)
+    tiny_number = 10**(-70.0)
+    return np.clip(res, a_min=tiny_number, a_max=None)
+
+
+#---------------- AGN torus emission ----------------
+
+SPS_HOME = os.getenv('SPS_HOME')
+Nenkova2008 = np.genfromtxt(os.path.join(SPS_HOME, 'dust', 'Nenkova08_y010_torusg_n10_q2.0.dat'),
+                            dtype=[('wave', 'f8'),
+                                   ('fnu_5', '<U20'), ('fnu_10', '<U20'), ('fnu_20', '<U20'), 
+                                   ('fnu_30', '<U20'), ('fnu_40', '<U20'), ('fnu_60', '<U20'), 
+                                   ('fnu_80', '<U20'), ('fnu_100', '<U20'), ('fnu_150', '<U20')],
+                            delimiter='   ', skip_header=4)
+agndust_tau = np.array([5.0, 10.0, 20.0, 30.0, 40.0, 60.0, 80.0, 100.0, 150.0])
+agndust_lam = Nenkova2008['wave']
+nagndust = agndust_tau.shape[0] # =9, number of optical depths for AGN dust models
+nagndust_spec = Nenkova2008['wave'].shape[0] # =125, number of spectral points in the input library
+
+agndust_specinit = [Nenkova2008['fnu_5'],  Nenkova2008['fnu_10'], Nenkova2008['fnu_20'],
+                    Nenkova2008['fnu_30'], Nenkova2008['fnu_40'], Nenkova2008['fnu_60'],
+                    Nenkova2008['fnu_80'], Nenkova2008['fnu_100'], Nenkova2008['fnu_150']]
+agndust_specinit = np.vstack(agndust_specinit).T
+agndust_specinit = np.array(agndust_specinit, dtype=np.float64)
+# agndust_specinit[agndust_lam<3e4,:] = 0
+
+def agn_torus(wave, agn_tau):
+    """AGN torus emission based on Nenkova+2008
+    wave: rest-frame wavelength in Angstrom
+    agn_tau: optical depth of the AGN dust torus, which affects the shape of the AGN SED;
+             outside the range (5, 150) the AGN SED is those at 5 and 150.
+
+    returns an AGN torus spectrum, interpolated on the input wavelength grid and agn_tau
+    """
+    agndust_spec = np.zeros((len(wave), nagndust), dtype=np.float64)
+
+    # interpolate data onto wave
+    i1 = np.argmin(np.abs(wave - agndust_lam[0]))
+    i2 = np.argmin(np.abs(wave - agndust_lam[nagndust_spec - 1]))
+    for i in range(nagndust):
+        agndust_spec[i1:i2 + 1, i] = 10**np.interp(np.log10(wave[i1:i2 + 1]),
+                                                   np.log10(agndust_lam),
+                                                   np.log10(agndust_specinit[:, i] + 1e-30)) - 1e-30
+
+    # interpolate in tau_agn
+    # extrapolate if outside the bounds
+    ## no extrapolation -- if outside the bounds, use the NN
+    linfit = interp1d(agndust_tau, agndust_spec, axis=1, bounds_error=False, fill_value='extrapolate')
+        # fill_value=(agndust_spec[:,0], agndust_spec[:,-1]))
+
+    agndust_speci = linfit(agn_tau)
+
+    return agndust_speci
+
+
